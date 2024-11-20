47287ba669b5ce6ec75f7180a0e51ec121e17016
piroggoff <102899075+piroggoff@users.noreply.github.com>
2024-11-20 21:36:06 +0300

    added connection to external psql server | cannot reach server/index.html

--- client/main.py
+++ client/main.py
@@ -1,10 +1,22 @@
 import urllib.request
+import psycopg2
+import json
 
-fp = urllib.request.urlopen("http://localhost:1234/index.html")
+with urllib.request.urlopen("http://localhost:1234/index.html") as fp:
+    encodedContent = fp.read()
+    decodedContent = encodedContent.decode("utf8")
+    print(decodedContent)
 
-encodedContent = fp.read()
-decodedContent = encodedContent.decode("utf8")
+with open("database_auth.json", "r") as json_db:
+    print("Database login info added.")
+    data = json.load(json_db)
+    json_db.close()
 
-print(decodedContent)
 
-fp.close()
+print("Doing a request to the database.")
+with psycopg2.connect(**data) as connection:
+    main_cur = connection.cursor()
+    main_cur.execute("SELECT * FROM phones;")
+    rows = main_cur.fetchall()
+    for row in rows:
+        print(row)
