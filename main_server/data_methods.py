a7833c7d2dbd76c96cb972f37019301fc52f7cb9
piroggoff <102899075+piroggoff@users.noreply.github.com>
2024-12-03 17:33:25 +0300

    env variables passing added

--- main_server/data_methods.py
+++ main_server/data_methods.py
@@ -1,5 +1,6 @@
 import psycopg2
-#import python-dotenv
+from dotenv import dotenv_values
+import os
 from pydantic import BaseModel
 
 class User(BaseModel):
@@ -7,8 +8,13 @@ class User(BaseModel):
     user: str
     password: str
     
+connection_data = { #Dictionary with DB auth info (can be overrided with runtime values)
+    #**os.environ
+    **dotenv_values(".env.secret")
+}
+
 def database_request(request: str = None) -> list: #Database request with env variables
-    with psycopg2.connect(host='localhost', port=5432, database='admin', user='admin', password='ptest') as connection:
+    with psycopg2.connect(**connection_data) as connection:
         cursor = connection.cursor()
         cursor.execute("SELECT column_name FROM information_schema.columns WHERE table_name = 'users'")
         columns = [elem[0] for elem in cursor.fetchall()] #Converting list[tuple] into list[]
@@ -17,7 +23,7 @@ def database_request(request: str = None) -> list: #Database request with env va
     print([dict(zip(columns, row)) for row in rows])
     return [dict(zip(columns, row)) for row in rows]
 
-def verify_logindata(login: str, password: str):
+def verify_logindata(login: str, password: str) -> bool:
     request = f"SELECT * FROM users as u WHERE u.login LIKE '{login}'"
     database_answer = database_request(request) 
     if len(database_answer) != 1: #Overflow or zero results 
@@ -30,3 +36,5 @@ def verify_logindata(login: str, password: str):
         if any([elem['password'] == password for elem in database_answer]): 
             return True
         return False
+
+#print(verify_logindata(login='Tarantino', password='skotina'))
